f005ce6ada410ceedf19d515c86e840baafa78d5
Michael <michaellittle92@gmail.com>
2025-11-24 09:24:00 +1100

    first commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+using System.Net.Mime;
+using To_Do_Api;
+
+//in memory db 
+var todos = new List<Todo>
+{
+    new Todo { Id = 1, Text = "Test text 01", IsDone = false },
+    new Todo { Id = 2, Text = "Test text 01", IsDone = false },
+    new Todo {Id = 3, Text = "Test text 01", IsDone = false }
+};
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1");
+        c.RoutePrefix = string.Empty;
+    });
+}
+
+
+var todoRoute = app.MapGroup("/api/todos");
+
+
+//GET /todos
+
+todoRoute.MapGet(string.Empty, () =>
+{
+    return Results.Ok(todos);
+});
+
+//GET /todos/{id}
+
+//POST /todos
+
+
+//PUT /todos/{id}
+
+//DELETE /todos/{id}
+
+app.Run();
---
+++ Todo.cs
@@ -0,0 +1,12 @@
+namespace To_Do_Api;
+
+public class Todo
+{
+    public int Id  { get; set; }
+    public required string Text { get; set; }
+    public bool IsDone  { get; set; }
+
+
+ 
+
+}
